87175af4d8a57002935ebcb5473b114671d337fc
elezermaster <megido17@gmail>
2021-10-04 17:04:18 +0300

    home svg animation added

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './home.css'
 import {ReactComponent as Cactus} from './cactus.svg'
+import spirit from 'spiritjs';
+import gsap from "gsap";
+
 const Home = () => {
+      spirit.loadAnimation({
+        loop: true,
+        path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/cactus.json',
+      }).then(() => {
+        gsap.set('svg', {autoAlpha: 1});
+      })
     return (
         <div className="svg-animation">
            <h1>Home under construction</h1>
            <Cactus/>
-           {/* <img src={Cactus} alt="cactus"/> */}
         </div>
     );
 };
